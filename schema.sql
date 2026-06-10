46699a79ee6eb57e27c6428a162d2bd6824caa68
Georgi Alvar Varavko <g.varavko@gmail.com>
2026-06-10 14:41:40 +0300

    update to gitignore

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE  plans (
+    id INTEGER PRIMARY KEY,
+    plan TEXT,
+    hours_per_week INTEGER,
+    info TEXT,
+    user_id REFERENCES users
+);
